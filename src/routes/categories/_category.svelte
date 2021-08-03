0dea27f669db8dac669f0f1f7446dae0e512911b
Mitchel <mitchel_jager@hotmail.com>
2021-08-03 16:52:28 +0200

    Add grades and category styling

--- src/routes/categories/_category.svelte
+++ src/routes/categories/_category.svelte
@@ -3,11 +3,19 @@
     title: null as string,
     type: null as string
   }
+
+  function categoryTypeIcon(type) {
+    if (type == "positive") return "👍"
+    if (type == "negative") return "👎"
+    if (type == "neutral") return "➖"
+  }
 </script>
 
 
 
 <div class="category category--{ category.type }">
+  <div class="category__icon">{ categoryTypeIcon(category.type) }</div>
+
   { category.title }
 </div>
 
@@ -15,8 +23,12 @@
 
 <style lang="scss">
   .category {
-    padding: .25rem .5rem;
-    color: white;
+    display: flex;
+    margin-bottom: .25rem;
+    padding: .35rem .5rem;
+    border-radius: .25rem;
+    line-height: 1.5rem;
+    color: hsla(0, 0, 100, .95);
     
     &--neutral {
       background: linear-gradient(to right, var(--border-color), transparent);
@@ -29,5 +41,13 @@
     &--negative {
       background: linear-gradient(to right, var(--red), transparent);
     }
+  }
+
+  .category__icon {
+    display: inline-block;
+    flex: 0 0 1.5rem;
+    width: 1.5rem;
+    margin-right: .25rem;
+    color: white;
   }
 </style>
--- src/routes/games/_game.svelte
+++ src/routes/games/_game.svelte
@@ -3,12 +3,31 @@
 
   export let game = {
     title: null as string,
+		publisher: null as string,
+		year_of_release: null as number,
     categories: null as []
   }
   export let loading = false
 
-	function sortCategories(categories) {
-		return categories.sort((a, b) => (a.type > b.type) ? 1 : -1)
+	$: grade = categoriesToScore()
+
+	function sortCategories() {
+		return game.categories.sort((a, b) => (a.type > b.type) ? 1 : -1).filter((c, i) => i < 10)
+	}
+
+	function categoriesToScore() {
+		let score:number = 10
+
+		const negativeCategories = game.categories?.filter(c => c.type == "negative").length
+
+		score -= (negativeCategories / 2)
+
+		if (score == 10) return "A"
+		if (score > 8) return "B"
+		if (score > 6) return "C"
+		if (score > 4) return "D"
+		if (score > 2) return "E"
+		return "F"
 	}
 </script>
 
@@ -18,10 +37,25 @@
   { #if !loading }
     <div class="card__header">
       <div class="card__image"></div>
-      <h3 class="card__title">{ game.title }</h3>
+
+			<div>
+				<h3 class="card__title">{ game.title }</h3>
+
+				{ #if game.year_of_release }
+					<div class="card__date">{ game.year_of_release }</div>
+				{ /if }
+
+				{ #if game.publisher }
+					<div class="card__name">{ game.publisher }</div>
+				{ /if }
+
+				<div class="card__grade card__grade--{ grade }">
+					{ grade }
+				</div>
+			</div>
     </div>
 
-		{ #each sortCategories(game.categories) as category }
+		{ #each sortCategories() as category }
 			<Category { category } />
 		{ /each }
   { /if }
@@ -38,9 +72,10 @@
   .card {
 		position: relative;
 		display: block;
+		height: 100%;
 		padding: 1.5rem;
 		border-radius: 1rem;
-		background: var(--content-bg);
+		background: var(--bg-dark);
 		box-shadow: var(--shadow-medium);
 
 		&--loading {
@@ -54,7 +89,7 @@
 				top: 0;
 				height: 100%;
 				width: 100%;
-				background: linear-gradient(to right, var(--content-bg) 10%, var(--body-bg), var(--content-bg) 90%);
+				background: linear-gradient(to right, var(--bg-dark) 10%, var(--content-bg), var(--bg-dark) 90%);
 				background-repeat: no-repeat;
 				animation: loading 1000ms infinite;
 			}
@@ -72,13 +107,54 @@
     width: 5rem;
     height: 7rem;
     margin-right: 1.5rem;
-    background: var(--bg-dark);
-		border: 1px solid var(--border-color);
+    background: var(--content-bg);
+		box-shadow: inset 0 0 0 1px var(--border-color);
   }
 
   .card__title {
     margin: 0;
+		width: 100%;
+		color: var(--text-color-title);
     font-size: 1.2rem;
-		color: var(--text-color-light);
   }
+
+	.card__date {
+		color: var(--text-color-dark);
+	}
+
+	.card__name {
+		color: var(--text-color-medium);
+	}
+
+	.card__grade {
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		padding: .15rem 0 .25rem;
+		margin-top: .5rem;
+		width: 2rem;
+		height: 2rem;
+		border-radius: 99px;
+		background: var(--border-color);
+		color: white;
+		font-size: 1.2rem;
+		line-height: 1em;
+		font-weight: bold;
+		text-shadow: 0 3px 1px hsla(0, 0, 0, .25);
+
+		$grades: (
+			A: var(--green),
+			B: var(--blue),
+			C: var(--border-color),
+			D: var(--yellow),
+			E: var(--orange),
+			F: var(--red)
+		);
+
+		@each $grade, $color in $grades {
+			&--#{ $grade } {
+				background: $color;
+			}
+		}
+	}
 </style>
