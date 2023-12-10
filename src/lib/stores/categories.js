e61e9aaa12d6d08263851582863ceb1a55223e87
Mitchel <mitchel_jager@hotmail.com>
2023-12-10 20:49:38 +0100

    fix: Sort categories by id after sorting by type

--- src/lib/stores/categories.js
+++ src/lib/stores/categories.js
@@ -1,3 +1,6 @@
 export function sortedCategories(categories) {
-	return categories.sort((a, b) => (a.type > b.type ? 1 : -1))
+	return categories.sort((a, b) => {
+		if (a.type !== b.type) return a.type > b.type ? 1 : -1
+		return a.id > b.id ? 1 : -1
+	})
 }
