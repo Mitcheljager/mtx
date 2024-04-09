c9737b93e4d5f3c3a8ad878cf2e4a5dbdeb05c21
Mitchel <mitchel_jager@hotmail.com>
2024-04-10 01:45:01 +0200

    chore: Use cloudflare adaptar

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess"
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-cloudflare"
 import { mdsvex } from "mdsvex"
 
 /** @type {import('@sveltejs/kit').Config} */
