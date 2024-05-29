89aacd27993ed219c5ad8e4f1eded8ae139194ac
Mitchel <mitchel_jager@hotmail.com>
2024-05-29 13:34:09 +0200

    chore: Remove semicolon

--- src/routes/image/[from]/[key]/+server.js
+++ src/routes/image/[from]/[key]/+server.js
@@ -1,4 +1,4 @@
-import { supabase } from "$lib/db";
+import { supabase } from "$lib/db"
 
 export async function GET({ params }) {
   const { data, error } = await supabase.storage.from(params.from).getPublicUrl(params.key)
