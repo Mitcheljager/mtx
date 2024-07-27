7dbeffa64c9d3cc03be5e621fbaa953429ea66cf
Mitchel <mitchel_jager@hotmail.com>
2024-07-27 20:33:56 +0200

    refactor: Add typing to utils

--- src/lib/utils/categories.ts
+++ src/lib/utils/categories.ts
@@ -1,4 +1,6 @@
-export function sortedCategories(categories) {
+import type { Category } from "$lib/types/Category"
+
+export function sortedCategories(categories: Category[]) {
 	return categories.sort((a, b) => {
 		if (a.type !== b.type) return a.type > b.type ? 1 : -1
 		return a.id > b.id ? 1 : -1
