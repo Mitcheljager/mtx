758f9cc3f638af7cb67597213b31967de5cd7f7d
Mitchel <mitchel_jager@hotmail.com>
2024-07-27 23:03:32 +0200

    fix: No longer catch in api endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,19 +1,14 @@
 import { cache } from "$lib/stores/cache"
 
-export async function api(path: string, serverFetch = null) : Promise<JSON | null> {
-  try {
-    const cachedInStore = cache.check(path)
-    if (cachedInStore) return cachedInStore
+export async function api(path: string, serverFetch = null) : Promise<any> {
+  const cachedInStore = cache.check(path)
+  if (cachedInStore) return cachedInStore
 
-    const response = await (serverFetch || fetch)(`/api/${path}`)
-    if (!response.ok) throw new Error()
+  const response = await (serverFetch || fetch)(`/api/${path}`)
+  if (!response.ok) throw new Error()
 
-    const parsed = await response.json()
-    cache.add(path, parsed)
+  const parsed = await response.json()
+  cache.add(path, parsed)
 
-    return parsed
-  } catch (error) {
-    console.error(error)
-    return null
-  }
+  return parsed
 }
