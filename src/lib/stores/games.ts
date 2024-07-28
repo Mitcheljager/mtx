1cdb33ad6f96120c7852f229719bab2cb5c0ca6e
Mitchel <mitchel_jager@hotmail.com>
2024-07-28 18:50:51 +0200

    chore: Remove unused function

--- src/lib/stores/games.ts
+++ src/lib/stores/games.ts
@@ -1,6 +1,5 @@
 import { writable, type Writable } from "svelte/store"
 
-import { supabase } from "$lib/db"
 import type { Game } from "$lib/types/Game"
 
 export const games: Writable<Game[]> = writable([])
@@ -12,14 +11,3 @@ export const gamesTable = "games"
 export const gamesSelect = `
   id, title, publisher, year_of_release, image_url, slug, tentative,
   categories (id, title, type)`
-
-export async function getSitemapData() : Promise<{ slug: string }[]> {
-	const { data, error } = await supabase
-		.from(gamesTable)
-		.select("slug")
-		.order("created_at", { ascending: false })
-
-	if (error) throw new Error(error.message)
-
-	return data
-}
