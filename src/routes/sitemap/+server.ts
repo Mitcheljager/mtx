95e792ca06dae92046ffa6bce511b4fbe095b2de
Mitchel <mitchel_jager@hotmail.com>
2025-05-19 23:18:47 +0200

    chore: Remove console.log

--- src/routes/sitemap/+server.ts
+++ src/routes/sitemap/+server.ts
@@ -17,8 +17,6 @@ export async function GET({ setHeaders } : RequestEvent) {
 		"Content-Type": "application/xml"
 	})
 
-  console.log(Math.ceil(data.length / itemsPerPage))
-
 	const body = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset
           xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
