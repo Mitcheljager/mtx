88edcd293692c8d1dbadc52c9a0bef3cc2b5120a
Mitchel <mitchel_jager@hotmail.com>
2025-09-28 14:18:36 +0200

    refactor: Get category and games in parallel

--- src/routes/categories/[id]/+page.ts
+++ src/routes/categories/[id]/+page.ts
@@ -7,17 +7,13 @@ import type { Game } from "$lib/types/Game";
 export const load : PageLoad = async ({ params, fetch }) => {
   const { id } = params;
 
-  let category;
-  let games;
-
-  try {
-    category = await api<Category>(`categories/${id}`, fetch);
-    if (!category) throw new Error;
-
-    games = await api<Game[]>(`games/category/${id}`, fetch);
-  } catch {
-    throw error(404, { message: "Not found" });
-  }
+  const [category, games] = (await Promise.allSettled([
+    api<Category>(`categories/${id}`, fetch),
+    api<Game[]>(`games/category/${id}`, fetch)
+  ])).map(p => {
+    // @ts-expect-error Shut up
+    return p.value
+  });
 
   return { category, games };
 };
