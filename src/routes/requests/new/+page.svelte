5c059cfe1ef7fc14d63e799c04d880e95e7fca6c
Mitchel <mitchel_jager@hotmail.com>
2025-09-28 13:21:04 +0200

    chore: Formatting

--- src/routes/requests/new/+page.svelte
+++ src/routes/requests/new/+page.svelte
@@ -18,13 +18,8 @@
 
       const response = await fetch("/api/requests/new", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          title,
-          description
-        })
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, description })
       });
 
       const text = await response.text();
